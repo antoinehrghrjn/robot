77e19a5759d7c7c188619f4954daa0c4f8d0d462
antoinehrghrjn <99180625+antoinehrghrjn@users.noreply.github.com>
2022-02-07 11:56:12 +0100

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,22 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 202)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 202)
+    }
+    if (receivedNumber == 2) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 202)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 202)
+    }
+    if (receivedNumber == 3) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 202)
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 0)
+    }
+    if (receivedNumber == 4) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 202)
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CCW, 0)
+    }
+    if (receivedNumber == 5) {
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+})
+radio.setGroup(1)
